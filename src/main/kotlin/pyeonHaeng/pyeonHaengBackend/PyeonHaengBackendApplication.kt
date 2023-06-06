9f64ed196b8d1a244c2c39913cc4f6f2d3e06091
sectionr0 <sectionr0@gmail.com>
2023-06-06 21:15:47 +0900

    Initial commit

---
+++ src/main/kotlin/pyeonHaeng/pyeonHaengBackend/PyeonHaengBackendApplication.kt
@@ -0,0 +1,11 @@
+package pyeonHaeng.pyeonHaengBackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PyeonHaengBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<PyeonHaengBackendApplication>(*args)
+}
---
+++ src/test/kotlin/pyeonHaeng/pyeonHaengBackend/PyeonHaengBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package pyeonHaeng.pyeonHaengBackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PyeonHaengBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
